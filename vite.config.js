27780aa4093fa8f39eb4a79724a5093a0d67cb6e
madortill <mador.till@gmail.com>
2024-06-17 17:09:42 +0300

    ddd

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-
 import vue from '@vitejs/plugin-vue'
-
-import { VitePWA } from 'vite-plugin-pwa';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: {
-      // 'vue': '@vue/compat',
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  plugins: [
-    vue({}),
-    VitePWA({
-      includeAssets: ['/*.png', '/bg/*'],
-      // Optional configuration options for VitePWA
-    }),
-  ],
   base: process.env.NODE_ENV === "production" ? "/astroid/" : "/",
-
 })
