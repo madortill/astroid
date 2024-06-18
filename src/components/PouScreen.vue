142045641eb090d3a79e5e2a95795f467ed86ef7
madortill <madortill@gmail.com>
2024-06-18 16:58:42 +0300

    l;

--- src/components/PouScreen.vue
+++ src/components/PouScreen.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="pou-screen" :style="{ 'background-image': 'url(' + backgroundImg + ')' }">
     <iframe class="animation2" v-show="showAnimation2" :src="playerAnimation"></iframe>
-    <iframe class="animation" v-if="showAnimation1" src="/public/animations/preloader/preloader.html"></iframe>
+    <iframe class="animation" v-if="showAnimation1" src="../public/animations/preloader/preloader.html"></iframe>
     <div v-else>
       <icons-for-pou :age="age" :level="level" v-show="!showAnimation2"></icons-for-pou>
        <audio ref="audio">
@@ -206,7 +206,7 @@ touchMove(ev) {
       return `../src/assets/media/pouScreen/${this.astroid}/${this.astroid}.svg`;
     },
     backgroundImg() {
-      return `C:/Users/MadorTil/Documents/astroid/src/assets/media/pouScreen/${this.astroid}/${this.astroid}Room.png`;
+      return `../src/assets/media/pouScreen/${this.astroid}/${this.astroid}Room.png`;
     },
     playerItem() {
       return `../src/assets/media/pouScreen/${this.astroid}/item.png`;
