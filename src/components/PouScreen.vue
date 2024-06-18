b9b6d9acab699db4d3d9f8783d1a81285daa8728
madortill <madortill@gmail.com>
2024-06-18 17:19:10 +0300

    gili firsr try

--- src/components/PouScreen.vue
+++ src/components/PouScreen.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="pou-screen" :style="{ 'background-image': 'url(' + backgroundImg + ')' }">
     <iframe class="animation2" v-show="showAnimation2" :src="playerAnimation"></iframe>
-    <iframe class="animation" v-if="showAnimation1" src="../../public/animations/preloader/preloader.html"></iframe>
+    <iframe class="animation" v-if="showAnimation1" src="/public/animations/preloader/preloader.html"></iframe>
     <div v-else>
       <icons-for-pou :age="age" :level="level" v-show="!showAnimation2"></icons-for-pou>
        <audio ref="audio">
@@ -200,16 +200,16 @@ touchMove(ev) {
   },
   computed: {
     playerAnimation() {
-      return `C:/Users/MadorTil/Documents/astroid/public/animations/${this.astroid}/${this.astroid}.html`;
+      return `/public/animations/${this.astroid}/${this.astroid}.html`;
     },
     playerImg() {
-      return `../src/assets/media/pouScreen/${this.astroid}/${this.astroid}.svg`;
+      return `../assets/media/pouScreen/${this.astroid}/${this.astroid}.svg`;
     },
     backgroundImg() {
-      return `C:/Users/MadorTil/Documents/astroid/src/assets/media/pouScreen/${this.astroid}/${this.astroid}Room.png`;
+      return `../assets/media/pouScreen/${this.astroid}/${this.astroid}Room.png`;
     },
     playerItem() {
-      return `../src/assets/media/pouScreen/${this.astroid}/item.png`;
+      return `../assets/media/pouScreen/${this.astroid}/item.png`;
     }
   },
   created() {
